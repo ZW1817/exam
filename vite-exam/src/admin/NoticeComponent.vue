19a1ea6e05bf3261eca616343a5effa06387a8e7
zgy <1817366401@qq.com>
2022-07-25 23:49:26 +0800

    second

---
+++ vite-exam/src/admin/NoticeComponent.vue
@@ -0,0 +1,61 @@
+<template>
+<el-main>
+  <el-form :model="form" label-width="90px" style="margin-top:30px">
+    <el-form-item label="标题">
+      <el-input v-model="form.name" />
+    </el-form-item>
+    
+    <el-form-item label="考试时间">
+      <el-col :span="11">
+        <el-date-picker
+          v-model="form.date1"
+          type="date"
+          placeholder="开始时间"
+          style="width: 100%"
+        />
+      </el-col>
+      <el-col :span="2" class="text-center">
+        <span class="text-gray-500">-</span>
+      </el-col>
+      <el-col :span="11">
+        <el-time-picker
+          v-model="form.date2"
+          placeholder="结束时间"
+          style="width: 100%"
+        />
+      </el-col>
+    </el-form-item>
+    <el-form-item label="内容">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">确定</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+  </el-main>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  date1: '',
+  date2: '',
+  desc: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+
+</script>
+<style scoped>
+.el-main{
+    width: 1200px;
+    height: 658px;
+    background-color: aliceblue;
+}
+</style>
---
+++ vite-exam/src/student/studentShow.vue
@@ -0,0 +1,39 @@
+<script>
+import Aside from './aside.vue'
+import Header from './header.vue'
+// import Answer from './student/answer.vue'
+import NotAnswer from './notAnswer.vue'
+  export default{
+    name:'App',
+    components: {
+    Header,
+    Aside,
+    // Answer
+    NotAnswer
+}
+}
+</script>
+
+<template>
+<el-container class="con"> 
+    <el-header>
+  <Header></Header>
+  </el-header>
+  <el-container style="height:350px">
+    <Aside></Aside>  
+  <router-view></router-view>
+  </el-container>
+</el-container> 
+</template>
+
+<style scoped>
+.el-main{
+  background-color: #f9f9f9;
+  color:#333;
+  /* text-align: center;
+  line-height: 160px; */
+}
+.con{
+    width:100%;
+}
+</style>
